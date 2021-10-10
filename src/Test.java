50e1f474ad37927fa1341f2dd5db749c04487897
LittleFlyers <2538285284@qq.com>
2021-10-10 17:43:15 +0800

    zpf的更新

--- src/Test.java
+++ src/Test.java
@@ -1,25 +1,12 @@
-
-import java.util.HashMap;
-import java.util.Map;
-
 public class Test {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 //        Map<String, Integer> map = new HashMap<String, Integer>();
 //        map.put("key1", 1);
 //        int n=map.get("key0");
-        String s="word";
-        System.out.println(s.substring(4,4).length());
-
-
-
-
-        System.out.println(s.substring(4,4).length());
-        System.out.println(s.substring(4,4).length());
-        System.out.println(s.substring(4,4).length());
-
-
+        String s = "word";
+        System.out.println(0);
     }
 }
 
